5e41c3bf02d7fad608b054f81659291ca064cb6e
Haziq <qwertyweee@gmail.com>
2020-05-23 14:40:18 +0800

    added GuestBlacklist table to database to track access denied face scanned. added GuestBlacklist class to guestmanagementapp models.

--- FaceRecTA/guestmanagementapp/models.py
+++ FaceRecTA/guestmanagementapp/models.py
@@ -7,11 +7,17 @@ class GuestDetails(models.Model):
     phone_number = models.IntegerField(null=False)
     nric = models.IntegerField(null=False, unique=True)
     comment = models.CharField(max_length=500, null=True)
-    valid_until = models.CharField(max_length=200, default='0')    
+    valid_until = models.CharField(max_length=200, default='0')
     status = models.IntegerField()
 
 class GuestAttendance(models.Model):
     capture_time = models.CharField(max_length=200)
+    capture_location = models.ForeignKey(TerminalDetails,default=000000 ,on_delete=models.SET_DEFAULT)
+    GuestDetails = models.ForeignKey(GuestDetails, on_delete=models.CASCADE, null=True)
+    temperature = models.CharField(max_length=100, default='')
+
+class GuestBlacklist(models.Model):
+    capture_time = models.CharField(max_length = 200)
     capture_location = models.ForeignKey(TerminalDetails,default=000000 ,on_delete=models.SET_DEFAULT)
     GuestDetails = models.ForeignKey(GuestDetails, on_delete=models.CASCADE, null=True)
     temperature = models.CharField(max_length=100, default='')
